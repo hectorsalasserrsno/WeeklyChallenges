771200caadd0ecb236a8871452f4b2034e94c2a1
Hector Salas Serrano <kenkira666@gmail.com>
2023-07-09 22:24:29 -0500

    Week 2 exercises done

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +9,140 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            if (char.IsLetter(c))
+            {
+                return true;
+            }
+            else
+            { 
+               return false;
+            }
+            
+         
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+
+            var count = vals.Length;
+            if (count % 2 == 0) 
+            {
+                return true;
+            }
+            
+            else
+            {
+                return false;
+            }
+            
+           
+            
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return number % 2 == 0;
         }
+    
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+        return num % 2 !=0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            
+            double max = double.MinValue;
+            double min = double.MaxValue;
+            foreach (double number in numbers) 
+            {
+                if(number > max)
+                {
+                    max = number;
+
+                }
+                if(number < min)
+                {
+                    min = number;
+                
+                }
+                
+            }
+            return max + min;
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length > str2.Length) 
+            {
+                return str2.Length;
+            }
+            else 
+            {
+                return str1.Length;
+            
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return  0;
+            }
+            int answer = 0;
+
+            for (int i  = 0; i < numbers.Length; i++)
+            { 
+                answer += numbers[i];
+            }
+            return answer;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) 
+            {
+                return 0;
+            }
+            int answer = 0;
+            foreach(int number in numbers)
+            {
+                if (number % 2 == 0) 
+                {
+                    answer += number;
+                }
+                
+            }  
+            return answer;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) 
+            {
+                return false;
+            }
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number; 
+                
+                           
+            }
+            return (sum % 2 != 0);
+
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            return number < 0 ? 0 : number / 2;
         }
     }
 }
