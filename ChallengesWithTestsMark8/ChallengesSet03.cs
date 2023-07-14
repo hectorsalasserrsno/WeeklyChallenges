1d7d710fd29451ed83f21f98b0d41eb85d624491
Hector Salas Serrano <kenkira666@gmail.com>
2023-07-14 08:33:27 -0500

    week 3 done

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +10,111 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (var val in vals) 
+            {
+                if (val == false)                // return vals != null && vals.Length != 0 && vals.contains(false);
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+
+            }
+                
+            int sum = 0;
+           foreach (var number in numbers) 
+            {
+                if(number % 2 != 0) 
+                {
+                    sum += number;
+                }
+            
+            }
+            if (sum % 2 != 0)
+            {
+                return true;
+
+            }
+            else 
+            {
+                 return false;
+            
+            }
+            //return numbers != null && numbers.Where(x =>x % 2 == 0).Sum() != 0;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+                //return password.Any(char.IsNumber)
+                //&& password.Any(char.IsUpper)
+                //&& password.Any(char.IsLower);
+            
+            bool isLetter = false;
+            bool isDigit = false;
+            bool isUpper = false;
+            foreach (var letter in password) 
+            {
+                if (char.IsLower(letter))
+                {
+                    isLetter = true;
+                
+                }
+                if (char.IsDigit(letter))
+                {
+                    isDigit = true;
+                
+                }
+                
+                if (char.IsUpper(letter)) 
+                {
+                    isUpper = true;
+                
+                }
+                
+            }
+            return isUpper && isLetter && isDigit;
+            
         }
-
+        
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[^1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            return divisor == 0 ? 0 : dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[^1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            return Enumerable.Range(0, 100).Where(x => x % 2 == 1).ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < words.Length; i++) 
+            {
+                words[i] = words[i].ToUpper();
+            
+            }
         }
     }
 }
